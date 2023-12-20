84cb24930f0bfe1874a42a8e789f3104ecdd0cdb
Barbi Adkins <badkins0043@gmail.com>
2023-12-20 13:23:43 -0500

    almost  working

--- reviews.py
+++ reviews.py
@@ -1,2 +1,20 @@
-# add your code here
+import pandas as pd
+
+# Unzip the CSV file
+!unzip winemag-data-130k-v2.csv.zip
+
+# Read the CSV file
+wine_reviews = pd.read_csv("winemag-data-130k-v2.csv")
+
+# Create country counts and points
+info = {
+    #"country": wine_reviews['country'].unique(),
+    "counts": wine_reviews['country'].value_counts(),
+    "points": wine_reviews.groupby('country')['points'].mean().round(1)
+}
+#read the dataframe
+output = pd.DataFrame(info)
+#save to csv file
+output.to_csv('reviews-per-country.csv', index=True)
+
 
